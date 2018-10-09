6ca1f0ab5832665c7797094de4878565021aace5
rossanooo <giocxanni24@gmail.com>
2018-10-09 10:48:07 +0200

    fine_esercizio_esattamente_una_coppia

--- src/EsattamenteUnaCoppia.java
+++ src/EsattamenteUnaCoppia.java
@@ -5,27 +5,19 @@ public class EsattamenteUnaCoppia
 	{
 		int [] vett = new int [10];
 		
-		RiempimentoDiUnArrayDi10ConRandom (vett);
+		RiempimentoDiUnArrayConRandom (vett);
 		
 		EsattamenteUnaCoppia (vett);
 		
-		StampaVettore (vett);
-		
+		StampaVettore (vett);	
 	}
 	
-	public static void RiempimentoDiUnArrayDi10ConRandom (int [] vet) 
+	public static void RiempimentoDiUnArrayConRandom (int [] vet) 
 	{
 		for (int i = 0; i < vet.length; i ++)
 			vet[i] = (int) (Math.random() * 101);	
 	}
-	public static void StampaVettore (int [] vet) 
-	{
-		for(int i = 0; i < vet.length; i ++)
-			if (i + 1 < vet.length)
-				System.out.print(vet[i] + ", ");
-			else
-				System.out.println(vet[i] + ".");
-	}
+	
 	public static void EsattamenteUnaCoppia (int [] vet) 
 	{
 		int coppie = 0;
@@ -36,5 +28,14 @@ public class EsattamenteUnaCoppia
 						coppie = coppie + 1;
 		if (coppie == 1)
 			System.out.println("Esattamente una coppia");
+	}
+	
+	public static void StampaVettore (int [] vet) 
+	{
+		for(int i = 0; i < vet.length; i ++)
+			if (i + 1 < vet.length)
+				System.out.print(vet[i] + ", ");
+			else
+				System.out.println(vet[i] + ".");
 	}
 }
